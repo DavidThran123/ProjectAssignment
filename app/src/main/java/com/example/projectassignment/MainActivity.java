64cb580dabf8f6638dba5470a2e8c29a0675786f
poppysomer <thra0024@algonquinlive.com>
2020-11-18 22:10:09 -0500

    Start of Project, use this commit for button access

--- app/src/main/java/com/example/projectassignment/MainActivity.java
+++ app/src/main/java/com/example/projectassignment/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.projectassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button david = findViewById(R.id.davidsButton);
+        david.setOnClickListener(v -> {
+            Intent goToAudio = new Intent(MainActivity.this, .class);
+            startActivity(goToAudio);
+
+        });
+        Button juhoon = findViewById(R.id.juhoonsButton);
+        juhoon.setOnClickListener(v -> {
+            Intent goToCovidCase = new Intent(MainActivity.this, .class);
+            startActivity(goToCovidCase);
+
+        });
+        Button nintendo = findViewById(R.id.nintendosButton);
+        nintendo.setOnClickListener(v -> {
+            Intent goToTicketMaster = new Intent(MainActivity.this, .class);
+            startActivity(goToTicketMaster);
+
+        });
+        Button matt = findViewById(R.id.mattsButton);
+        matt.setOnClickListener(v -> {
+            Intent goToRecipe = new Intent(MainActivity.this, .class);
+            startActivity(goToRecipe);
+
+        });
+
     }
 }
