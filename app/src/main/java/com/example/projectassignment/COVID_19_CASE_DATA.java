28e7ec1beb1d6cf27e0eadde4d23e51aa75bb3c3
Juhoon Park <park0519@algonquinlive.com>
2020-11-25 17:51:46 -0500

    created Covid.java, its layout

--- app/src/main/java/com/example/projectassignment/COVID_19_CASE_DATA.java
+++ app/src/main/java/com/example/projectassignment/COVID_19_CASE_DATA.java
@@ -32,11 +32,54 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class COVID_19_CASE_DATA extends AppCompatActivity {
+    TextView caseTitle;
+    EditText caseSearch;
+    Button searchButton;
+    ProgressBar caseBar;
+    TextView caseResult;
+    ListView covidListView;
+    public int covidCase;
+    public Bundle dataToPassFred;
+    public ArrayList<Covid> covidArrayList = new ArrayList<>();
+    public CovidListAdapter covidListAdapter = new CovidListAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_covid_case_data);
 
+        covidListView = findViewById(R.id.caseListView);
+        covidListView.setAdapter(covidListAdapter);
+
+    }
+
+private class CovidListAdapter extends BaseAdapter{
+    @Override // number of items in the list
+    public int getCount() {
+        return  covidArrayList.size();
+    }
+
+    @Override // what string goes at row i
+    public Object getItem(int i) {
+        return covidArrayList.get(i);
+    }
+
+    @Override //database id of item at row i
+    public long getItemId(int i) {
+        return covidArrayList.get(i).getId();
+    }
+
+
+    @Override //controls which widgets are on the row
+    public View getView(int i, View old, ViewGroup parent)
+        {
+            LayoutInflater inflater = getLayoutInflater();
+            View newView = inflater.inflate(R.layout.activity_covid, parent, false);
+            TextView textview = newView.findViewById(R.id.covidTitle);
+            Covid thisCovid = (Covid) getItem(i);
+            textview.setText(thisCovid.getCountryName());
+            return newView;
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/projectassignment/Covid.java
@@ -0,0 +1,5 @@
+package com.example.projectassignment;
+
+public class Covid {
+
+}
