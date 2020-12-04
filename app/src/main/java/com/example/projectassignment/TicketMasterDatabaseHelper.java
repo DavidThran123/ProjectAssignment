fe4edfa5cd72c197c2a485260cb02b7d4cd6f04e
Ding Sun <sun00093@algnquinlive.com>
2020-12-04 11:56:32 -0500

    Start

--- app/src/main/java/com/example/projectassignment/TicketMasterDatabaseHelper.java
+++ app/src/main/java/com/example/projectassignment/TicketMasterDatabaseHelper.java
@@ -3,8 +3,10 @@ package com.example.projectassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 
 public class TicketMasterDatabaseHelper extends SQLiteOpenHelper {
 
@@ -13,20 +15,58 @@ public class TicketMasterDatabaseHelper extends SQLiteOpenHelper {
 
     public final static String TABLE_NAME = "Events";
 
-    public final static String col1 = "Title";
-    public final static String col2 = "Date";
-    public final static String col3 = "MinPrice";
-    public final static String col4 = "MaxPrice";
-    public final static String col5 = "URL";
-    public final static String col6 = "ImageURL";
+    public final static String col1 = "ID";
+    public final static String col2 = "Title";
+    public final static String col3 = "Date";
+    public final static String col4 = "MinPrice";
+    public final static String col5 = "MaxPrice";
+    public final static String col6 = "URL";
+    public final static String col7 = "ImageURL";
 
     TicketMasterDatabaseHelper(Context context){
+        super(context, DATABASE_NAME,null,VERSION_NUM);
 
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(SQLiteDatabase db){
+        //String for creating the Albums table
+        String createTable;
+        createTable = "CREATE TABLE " +TABLE_NAME+"(" + col1+" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                col2+" INTEGER , " +
+                col3+" TEXT, " +
+                col4+" INTEGER, " +
+                col5+" TEXT, " +
+                col6+" TEXT, " +
+                col7+" TEXT)";
+
+
+        //Executing both strings
+        db.execSQL(createTable);
 
     }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + oldVersion + " newVersion=" + newVersion);
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate(db);
+
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        Log.i("ChatDatabaseHelper", "Calling onDowngrade, oldVersion=" + oldVersion + " newVersion=" + newVersion);
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void deleteAll()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("delete from "+ TABLE_NAME);
+        db.close();
+    }
+
 }
