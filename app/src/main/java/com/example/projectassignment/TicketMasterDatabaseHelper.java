2b3e4378a498a1ae83f4aafb2b9eb31908075869
Ding Sun <sun00093@algnquinlive.com>
2020-12-03 22:51:54 -0500

    Start

--- app/src/main/java/com/example/projectassignment/TicketMasterDatabaseHelper.java
+++ app/src/main/java/com/example/projectassignment/TicketMasterDatabaseHelper.java
@@ -19,6 +19,6 @@ public class TicketMasterDatabaseHelper extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ticket_master_database_helper);
+
     }
 }
